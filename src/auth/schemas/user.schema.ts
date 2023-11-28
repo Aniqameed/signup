eaa1ccc681d510c9637f3bb2a06559b1f6bb605c
Aniqameed <aniqameed078@gmail.com>
2023-11-28 09:32:30 +0530

    first commit

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,23 @@
+import { SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema } from "@nestjs/mongoose";
+
+
+
+
+@Schema({
+    timestamps: true,
+})
+
+export class User{
+    @Prop()
+    name:string;
+
+    @Prop({unique: [true, 'Duplicate email entered']})
+    email:string;
+
+    @Prop()
+    password:string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
